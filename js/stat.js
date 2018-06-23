f064a8c393d33e31ef88523f26c5cee3042d2bbf
Lina <Lina.itt@gmail.com>
2018-06-23 10:02:55 +0800

    Заменены одинарные кавычки на двойные в stat.js

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById("canvas");
 if (canvas.getContext) {
-  var ctx = canvas.getContext('2d')
+  var ctx = canvas.getContext("2d")
 };
 //Функция переноса и вывода текста, если он не помещается в заданном поле
 function wrapAndRenderText(ctx, text, marginLeft, marginTop, maxWidth, lineHeight) {
@@ -32,34 +32,34 @@ for (var i = 0; i < Arr.length; i++) {
 };
 //Функция получения случайного оттенка голубого цвета
 function getRandomBlueColor() {
-  var randomRGBA = Math.floor(Math.random() * 50);
-  return 'rgba((9 + randomRGBA), (212 + randomRGBA), (227 +randomRGBA), 1)';
+  var randomRGBA = Math.floor(Math.random() * 10);
+  return "rgba((9 + randomRGBA), (212 + randomRGBA), (227 +randomRGBA), 1)";
 };
 //Функция построения столбцов гистограммы
 function renderHistogram(times, names) {
+  var colorRec;
+  var maxTimes = maxArrItem(times); // находим макс.время, соответствующее макс.высоте столбца гистограммы 150px
+  var height = Math.round(150 * times[i] / maxTimes); //калибруем относительно maxTimes
   for (var i = 0; i < times.length; i++) {
-    if (names[i] = 'Вы') {
-      var colorRec = 'rgba(255, 0, 0, 1)' //цвет столбца только для игрока - Вы
+    if (names[i] = "Вы") {
+      colorRec = "rgba(255, 0, 0, 1)" //цвет столбца только для игрока - Вы
     } else {
-      var colorRec = getRandomBlueColor(); //различные оттенки голубого для остальных игроков
+      colorRec = getRandomBlueColor(); //различные оттенки голубого для остальных игроков
     }
     ctx.fillStyle = colorRec;
-    // Находим максимальное время, соответствующее максимальной высоте столбца гистограммы 150px
-    var maxTimes = maxArrItem(times);
-    var height = Math.round(150 * times[i] / maxTimes); //калибруем относительно maxTimes
-    ctx.fillRect(150 + i * 90, 230 - Height, 190 + i * 90, 230)
+    ctx.fillRect(150 + i * 90, 230 - height, 190 + i * 90, 230)
   }
 };
 //Функция вывода текста сверху и снизу столбцов гистограммы
 function renderHistogramText(times, height) {
-  ctx.fillText('times[i]', 150 + i * 90, 220 - height);
-  ctx.fillText('names[i]', 150 + i * 90, 250)
+  ctx.fillText("times[i]", 150 + i * 90, 220 - height);
+  ctx.fillText("names[i]", 150 + i * 90, 250)
 };
 //Функция построения статистики игроков
 function renderStatistics(ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
   ctx.fillRect(110, 20, 530, 290); //тень от облака
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = "white";
   ctx.fillRect(100, 10, 520, 280); //белое облако сверху тени
   var MAX_WIDTH = 420; //ширина поля, где выводится текст
   var lineHeight = 22; //берем высоту строки 16px + межстрочный интервал 6px
