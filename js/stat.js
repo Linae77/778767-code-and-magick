963c670c5d8a82ab81cf7a7de1db803d6e1f7cbd
Lina <Lina.itt@gmail.com>
2018-07-01 21:52:51 +0300

    Оптимизированы функции, изменена window.renderStatistics

--- js/stat.js
+++ js/stat.js
@@ -1,84 +1,86 @@
 'use strict';
-// Функция переноса и вывода текста, если он не помещается в заданном поле
-(function wrapAndRenderText(context, text, marginLeft, marginTop, maxWidth, lineHeight) {
-  var words = text.split(' ');
-  var line = '';
-  for (var n = 0; n < words.length; n++) {
-    var testLine = line + words[n] + ' ';
-    var testWidth = context.measureText(testLine).width;
-    if (testWidth > matxWidth) {
-      context.fillText('', marginLeft, marginTop);
-      line = words[n] + ' ';
-      marginTop += lineHeight;
-    } else {
-      line = testLine;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var COLOR_WHITE = '#fff';
+var COLOR_GRAY = 'rgba(0, 0, 0, 0.7)';
+var COLOR_RED = 'rgba(255, 0, 0, 1)';
+var COLOR_BLACK = '#000';
+var GAP = 10;
+var TEXT_HEIGHT = 90;
+var TEXT_FONT = '16px PT Mono';
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var MAX_BAR_HEIGHT = 150;
+var BAR_MARGIN_LEFT = (BAR_WIDTH + BAR_GAP);
+var CLOUD_PADDING_TOP = MAX_BAR_HEIGHT + TEXT_HEIGHT;
+
+// Функция поиска максимума в массиве
+var getMaxItem = function (items) {
+  var maxItem = items[0];
+  for (var i = 0; i < items.length; i++) {
+    if (maxItem < items[i]) {
+      maxItem = items[i];
     }
   }
-  context.fillText('', marginLeft, marginTop);
-  return wrapAndRenderText;
-})();
+  return maxItem;
+};
 
-// Функция получения случайного оттенка голубого цвета
-function getRandomBlueColor() {
+// IIFE функция получения случайного оттенка голубого цвета
+(function getRandomBlueColor() {
   var randomRGBA = Math.floor(Math.random() * 50);
   var a = 9 + randomRGBA;
   var b = 212 + randomRGBA;
   var c = 227 + randomRGBA;
-  var rgba = {a: a, b: b, c: c};
-  return rgba.a + ', ' + rgba.b + ', ' + rgba.c + ', ' + 1; // строка
+  return 'rgba(' + a + ', ' + b + ', ' + c + ', 1)';
+})();
+
+// Генерация облака результатов
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-// Функция построения столбцов гистограммы
-(function renderHistogram(times, names) {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-  }
-  // Функция поиска максимума в массиве
-  var maxArrItem = function (items) {
-    var maxItem = items[0];
-    for (var i = 0; i < items.length; i++) {
-      if (maxItem < items[i]) {
-        maxItem = items[i];
-      }
-    }
-    return maxItem;
+
+// Генерация столбца гистограммы
+var renderHistogram = function (ctx, name, x, y) {
+  ctx.fillStyle = (name === 'Вы') ? COLOR_RED : getRandomBlueColor();
+  ctx.fillRect(CLOUD_X + x, (CLOUD_PADDING_TOP + GAP) - y, BAR_WIDTH, y);
+};
+
+// Генерация времени в секундах
+var renderTime = function (ctx, time, x, y) {
+  ctx.fillText(Math.round(time), CLOUD_X + x, (CLOUD_PADDING_TOP - GAP) - y);
+};
+
+// Генерация имен игроков
+var renderName = function (ctx, name, x) {
+  ctx.fillStyle = COLOR_BLACK;
+  ctx.fillText(name, CLOUD_X + x, CLOUD_Y + CLOUD_HEIGHT - GAP * 2);
+};
+
+// Генерация текста список результатов
+  var renderText = function (ctx, font, color) {
+    ctx.font = font;
+    ctx.fillStyle = color;
+    ctx.textBaseline = 'hanging';
+    ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
+    ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
   };
-  for (var i = 0, colorRec; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      colorRec = 'rgba(255, 0, 0, 1)'; // цвет столбца только для игрока - Вы
-    } else {
-      colorRec = getRandomBlueColor(); // различные оттенки голубого для остальных игроков
-    }
-    ctx.fillStyle = colorRec;
-    // Находим максимальное время, соответствующее максимальной высоте столбца гистограммы 150px
-    var maxTimes = maxArrItem(times);
-    var height = Math.round(150 * times[i] / maxTimes); // калибруем относительно maxTimes
-    ctx.fillRect(150 + i * 90, 230 - height, 190 + i * 90, 230);
-    ctx.fillText('times[i]', 150 + i * 90, 220 - height); // выводим подписи к столбцам
-    ctx.fillText('names[i]', 150 + i * 90, 250);
-  }
-  return renderHistogram;
-})();
+
 // Функция построения статистики игроков
-(function renderStatistics (context, names, times) {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+window.renderStatistics = function (ctx, names, times) {
+  var maxTime = getMaxItem(times);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, COLOR_GRAY); // тень от облака
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, COLOR_WHITE);
+  renderText(ctx, TEXT_FONT, COLOR_BLACK);
+  // Гистограмма
+  for (var i = 0; i < names.length; i++) {
+    var barWidth = BAR_GAP + BAR_MARGIN_LEFT * i;
+    var barHeight = MAX_BAR_HEIGHT * times[i] / maxTime;
+    // Время в секундах
+    renderTime(ctx, times[i], barWidth, barHeight);
+    renderHistogram(ctx, names[i], barWidth, barHeight);
+    renderName(ctx, names[i], barWidth);
   }
-  context.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  context.fillRect(110, 20, 530, 290); // тень от облака
-  context.fillStyle = 'white';
-  context.fillRect(100, 10, 520, 280); // белое облако сверху тени
-  var MAX_WIDTH = 420; // ширина поля, где выводится текст
-  var lineHeight = 22; // берем высоту строки 16px + межстрочный интервал 6px
-  var MARGIN_LEFT = 110;
-  var MARGIN_TOP = 20;
-  var text = 'Ура вы победили!\nСписок результатов:';
-  context.font = '16px PT Mono';
-  context.fillStyle = 'black';
-  // выводим текст на облаке
-  wrapAndRenderText(ctx, text, MARGIN_LEFT, MARGIN_TOP, MAX_WIDTH, lineHeight);
-  // рыисуем столбцы гистограммы, высота которых соответствует времени из массива times, отступаем по 50px от краев
-  renderHistogram(times, names);
-  return renderStatistics;
-})();
+};
